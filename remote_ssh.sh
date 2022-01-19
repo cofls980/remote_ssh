0ccd203b1317c5a39e71ae6b5c52f65fa43747aa
Chaerin Kim <chaekim@c1r2s6.42seoul.kr>
2022-01-20 00:38:19 +0900

    first commit

---
+++ remote_ssh.sh
@@ -0,0 +1,1 @@
+ssh -o StrictHostKeyChecking=no -i ~/remote_ssh/mykey -fNR 4321:localhost:8080 ubuntu@129.154.216.157
---
+++ ubuntu_connect.sh
@@ -0,0 +1,2 @@
+chmod 400 ~/remote_ssh/mykey
+ssh -o StrictHostKeyChecking=no -i ~/remote_ssh/mykey ubuntu@129.154.216.157
